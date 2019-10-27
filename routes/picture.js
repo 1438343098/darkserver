9e42b3dfad1ca8a08feaaf9615665979a97f825d
刘汉良 <1438343098@qq.com>
2019-10-27 23:42:05 +0800

    first commit

---
+++ routes/picture.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var https = require("https")
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+	if (req.query["text"]) {
+		var urls = "https://api.bilibili.com/x/web-interface/search/type?search_type=photo&highlight=1&keyword=" + req.query.text+'&page='+ req.query.page
+	} else {
+		var urls =
+			"https://api.bilibili.com/x/web-interface/search/type?search_type=photo&highlight=1&keyword=%E5%9B%BE%E7%89%87&page="+ req.query.page
+	}
+
+	let datas = ""
+	console.log(urls)
+	https.get(urls, function(okdata) {
+		okdata.setEncoding("utf8")
+		okdata.on("data", chunk => datas += chunk)
+		okdata.on("end", () => res.end(datas))
+	})
+});
+router.get('/listInfo', function(req, res, next) {
+	console.log(req.query.doc_id)
+	var urls = "https://api.vc.bilibili.com/link_draw/v1/doc/detail?doc_id=" + req.query.doc_id
+	let datas = ""
+	https.get(urls, function(okdata) {
+		okdata.setEncoding("utf8")
+		okdata.on("data", chunk => datas += chunk)
+		okdata.on("end", () => res.end(datas))
+	})
+});
+
+module.exports = router;
