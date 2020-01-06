4506d8d93df9d8f52f42348dd74f86e1834711e2
刘汉良 <1438343098@qq.com>
2020-01-06 21:37:26 +0800

    webhook

--- webhook/index.js
+++ webhook/index.js
@@ -1,7 +1,17 @@
-var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: 'liu19971026' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+});
+
 
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
@@ -11,14 +21,6 @@ function run_cmd(cmd, args, callback) {
   child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
   child.stdout.on('end', function() { callback (resp) });
 }
-
-http.createServer(function (req, res) {
-	console.log(5555)
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(7777)
 
 handler.on('error', function (err) {
   console.error('Error:', err.message)
