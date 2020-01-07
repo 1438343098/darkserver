6f5200693bc67bcd8927d1320a7eee74c4901486
laoliu <1438343098@qq.com>
2020-01-07 13:25:31 +0800

    修改写法

--- webhook/index.js
+++ webhook/index.js
@@ -1,23 +1,31 @@
 var http = require('http')
-var createHandler = require('node-github-webhook')
-var handler = createHandler({ path: '/push', secret: 'liu19971026' }) // 单个仓库
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/webhooks', secret: 'liu19971026' })
+// 上面的 secret 保持和 GitHub 后台设置的一致
+
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
   })
-}).listen(6606)
+}).listen(7777)
 
 handler.on('error', function (err) {
   console.error('Error:', err.message)
 })
 
-handler.on('*', function (event) {
-  console.log(
-    'Received a push event for %s to %s',
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref,
-	event.path
-  )
+    event.payload.ref);
+  run_cmd('sh', ['./index.sh'], function(text){ console.log(text) });
 })
