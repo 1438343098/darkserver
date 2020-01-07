33fba72d8241780406ebef8800903a7312af82bb
laoliu <1438343098@qq.com>
2020-01-07 12:24:28 +0800

    修改

--- webhook/index.js
+++ webhook/index.js
@@ -1,31 +1,30 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/push', secret: 'liu19971026' })
-// 上面的 secret 保持和 GitHub 后台设置的一致
+// install with: npm install @octokit/webhooks
+const WebhooksApi = require("@octokit/webhooks");
+const webhooks = new WebhooksApi({
+  secret: "liu19971026"
+});
 
-function run_cmd(cmd, args, callback) {
-  var spawn = require('child_process').spawn;
-  var child = spawn(cmd, args);
-  var resp = "";
+const webhookProxyUrl = "http://superliu.cn:6606/push"; // replace with your own Webhook Proxy URL
+const source = new EventSource(webhookProxyUrl);
+source.onmessage = event => {
+  const webhookEvent = JSON.parse(event.data);
+  webhooks
+    .verifyAndReceive({
+      id: webhookEvent["x-request-id"],
+      name: webhookEvent["x-github-event"],
+      signature: webhookEvent["x-hub-signature"],
+      payload: webhookEvent.body
+    })
+    .catch(console.error);
+};
 
-  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
-  child.stdout.on('end', function() { callback (resp) });
-}
+webhooks.on("push", ({ id, name, payload }) => {
+  console.log(name, "event received");
+});
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(6606)
 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
 
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref);
-    run_cmd('sh', ['./index.sh',event.payload.repository.name], function(text){ console.log(text) });
-})
+require("http")
+  .createServer(webhooks.middleware)
+  .listen(6606);
+// can now receive webhook events at port 6606
