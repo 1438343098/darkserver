78dc4fa0640d969cbca74dd518d1f1f74c7ab3fb
刘汉良 <1438343098@qq.com>
2020-01-08 21:59:55 +0800

    修改成都撒打发斯蒂芬

--- webhook/index.js
+++ webhook/index.js
@@ -1,9 +1,7 @@
 let time = null
-
 time = setInterval(()=>{
 	run_cmd('sh', ['./index.sh'], function(text){ console.log(text) });
 },1000 * 60 * 2)
-
 function run_cmd(cmd, args, callback) {
 	console.log("执行了nodejs定时任务")
   var spawn = require('child_process').spawn;
