661f1889614f1b541bf2bd2b8a4c3a9ab28e6002
刘汉良 <1438343098@qq.com>
2020-01-08 21:57:02 +0800

    修改

--- webhook/index.js
+++ webhook/index.js
@@ -2,7 +2,7 @@ let time = null
 
 time = setInterval(()=>{
 	run_cmd('sh', ['./index.sh'], function(text){ console.log(text) });
-},1000 * 60 * 1)
+},1000 * 60 * 2)
 
 function run_cmd(cmd, args, callback) {
 	console.log("执行了nodejs定时任务")
