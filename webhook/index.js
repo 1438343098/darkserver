10895c73ed75addf5184626903a13a3853b0e47f
刘汉良 <1438343098@qq.com>
2020-01-08 21:16:49 +0800

    添加打印

--- webhook/index.js
+++ webhook/index.js
@@ -2,9 +2,11 @@ const { exec } = require('child_process');
 const shellOrder = 'sh ./index.sh';
 const schedule = require('node-schedule')
 schedule.scheduleJob('60 * * * * *', () => {
+	console.log('定时任务执行了')
   exec(shellOrder, (err, stdout, stderr) => {
     if(err) {
       console.log('err: ', err)
     }
+	console.log('定时执行成功')
   })
 })
